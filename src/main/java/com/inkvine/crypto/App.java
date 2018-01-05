499cfe007eff4a8472cc90ee3775561b0f991df0
Groneberg, Jeffrey <jeffrey.groneberg@sap.com>
2018-01-05 15:34:04 +0100

    Initial commit

--- src/main/java/com/inkvine/crypto/App.java
+++ src/main/java/com/inkvine/crypto/App.java
@@ -6,6 +6,7 @@ import com.inkvine.crypto.rest.CoinMarketCapAPI;
 
 import de.vandermeer.asciitable.AsciiTable;
 import de.vandermeer.skb.interfaces.document.TableRowStyle;
+import org.apache.commons.lang3.ObjectUtils;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
@@ -65,62 +66,79 @@ public class App {
             @Override
             public void run() {
 
-                AsciiTable at = new AsciiTable();
-                at.getContext().setWidth(130);
-                at.addRule();
-                at.addRow("Coin Name", "Number of", "Value", "Value all", "Change %", "Change Value");
+                try {
 
-                Map<String, Double> valueOfWallet = getValueOfWallet(coinMarketCapAPI, finalWallet, verbose);
+                    AsciiTable at = new AsciiTable();
+                    at.getContext().setWidth(130);
+                    at.addRule();
+                    at.addRow("Coin Name", "Number of", "Value", "Value all", "Change %", "Change Value");
 
-                double cumulatedValueOld = oldValues != null && !oldValues.isEmpty() && oldValues.size() > 0 ? oldValues.keySet().stream().mapToDouble(s -> {
+                    Map<String, Double> valueOfWallet = getValueOfWallet(coinMarketCapAPI, finalWallet, verbose);
 
-                    return oldValues.get(s) * finalWallet.get(s);
+                    double cumulatedValueOld = oldValues != null && !oldValues.isEmpty() && oldValues.size() > 0 ? oldValues.keySet().stream().mapToDouble(s -> {
 
-                }).sum() : 0;
+                        return oldValues.get(s) * finalWallet.get(s);
 
-                valueOfWallet.keySet().forEach(s -> {
+                    }).sum() : 0;
 
-                    double percentageChange = 0;
-                    double difference = 0;
+                    valueOfWallet.keySet().forEach(s -> {
 
-                    if (!oldValues.isEmpty() && oldValues.containsKey(s)) {
-                        percentageChange = (valueOfWallet.get(s) - oldValues.get(s)) / oldValues.get(s) * 100;
-                        difference = valueOfWallet.get(s) - oldValues.get(s);
+                        double percentageChange = 0;
+                        double difference = 0;
+
+                        if (!oldValues.isEmpty() && oldValues.containsKey(s)) {
+                            percentageChange = (valueOfWallet.get(s) - oldValues.get(s)) / oldValues.get(s) * 100;
+                            difference = valueOfWallet.get(s) - oldValues.get(s);
+                        }
+
+                        at.addRule();
+                        at.addRow(s, finalWallet.get(s), valueOfWallet.get(s), valueOfWallet.get(s) * finalWallet.get(s), percentageChange, difference);
+
+                        // copy values for next run
+                        oldValues.put(s, valueOfWallet.get(s));
+
+                    });
+
+                    String table = at.render();
+                    System.out.println(table);
+
+                    double cumulatedValueNew = 0;
+                    try {
+                        cumulatedValueNew = (finalWallet.keySet().stream().mapToDouble(s -> {
+
+                            try {
+                                return (valueOfWallet.containsKey(s) ? valueOfWallet.get(s) : 0) * finalWallet.get(s);
+                            } catch (NullPointerException e) {
+                                return 0;
+                            }
+
+                        })).sum();
+
+                    } catch (NullPointerException e) {
+
                     }
 
-                    at.addRule();
-                    at.addRow(s, finalWallet.get(s), valueOfWallet.get(s), valueOfWallet.get(s) * finalWallet.get(s), percentageChange, difference);
+                    double walletPercentageChange = ((cumulatedValueNew - cumulatedValueOld) / cumulatedValueOld * 100);
+                    double walletDifference = cumulatedValueNew - cumulatedValueOld;
 
-                    // copy values for next run
-                    oldValues.put(s, valueOfWallet.get(s));
+                    System.out.println("\n" + ConsoleColors.BLUE_BOLD_BRIGHT + "New cumulated value of your wallet: " + ((walletPercentageChange >= 0) ? ConsoleColors.GREEN_BOLD_BRIGHT : ConsoleColors.RED_BOLD_BRIGHT) +
 
-                });
+                            cumulatedValueNew
 
-                String table = at.render();
-                System.out.println(table);
-
-                double cumulatedValueNew = (finalWallet.keySet().stream().mapToDouble(s -> {
-
-                    return (valueOfWallet.containsKey(s) ? valueOfWallet.get(s) : 0) * finalWallet.get(s);
-
-                })).sum();
-
-                double walletPercentageChange = ((cumulatedValueNew - cumulatedValueOld) / cumulatedValueOld * 100);
-                double walletDifference = cumulatedValueNew - cumulatedValueOld;
-
-                System.out.println("\n" + ConsoleColors.BLUE_BOLD_BRIGHT + "New cumulated value of your wallet: " + ((walletPercentageChange >= 0) ? ConsoleColors.GREEN_BOLD_BRIGHT : ConsoleColors.RED_BOLD_BRIGHT) +
-
-                        cumulatedValueNew
-
-                        + ConsoleColors.BLUE_BOLD_BRIGHT + " EUR" + ConsoleColors.BLUE_BOLD_BRIGHT + " --- Old Value of your wallet: " + cumulatedValueOld + ConsoleColors.RESET);
-                System.out.println(ConsoleColors.BLUE_BOLD_BRIGHT
-                        + "% Change till last calculation: "
-                        + ((walletPercentageChange >= 0) ? ConsoleColors.GREEN_BOLD_BRIGHT : ConsoleColors.RED_BOLD_BRIGHT)
-                        + walletPercentageChange + ConsoleColors.BLUE_BOLD_BRIGHT
-                        + " --- Value change till last update: " + ((walletPercentageChange >= 0) ? ConsoleColors.GREEN_BOLD_BRIGHT : ConsoleColors.RED_BOLD_BRIGHT)
-                        + walletDifference + ConsoleColors.BLUE_BOLD_BRIGHT + " EUR" + ConsoleColors.RESET);
+                            + ConsoleColors.BLUE_BOLD_BRIGHT + " EUR" + ConsoleColors.BLUE_BOLD_BRIGHT + " --- Old Value of your wallet: " + cumulatedValueOld + ConsoleColors.RESET);
+                    System.out.println(ConsoleColors.BLUE_BOLD_BRIGHT
+                            + "% Change till last calculation: "
+                            + ((walletPercentageChange >= 0) ? ConsoleColors.GREEN_BOLD_BRIGHT : ConsoleColors.RED_BOLD_BRIGHT)
+                            + walletPercentageChange + ConsoleColors.BLUE_BOLD_BRIGHT
+                            + " --- Value change till last update: " + ((walletPercentageChange >= 0) ? ConsoleColors.GREEN_BOLD_BRIGHT : ConsoleColors.RED_BOLD_BRIGHT)
+                            + walletDifference + ConsoleColors.BLUE_BOLD_BRIGHT + " EUR" + ConsoleColors.RESET);
 
 
+                } catch (Throwable t) {
+
+                    System.out.println("Something went wrong this time :)");
+
+                }
             }
         };
 
